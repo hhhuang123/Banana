ba3d1932098531eb3a0bb92ea547e30dc8f45f29
hhh <1647805407@qq.com>
2024-04-04 20:57:35 +0800

    test

--- banana.py
+++ banana.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import errno
 from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QGridLayout, QLabel, 
     QSizePolicy, QVBoxLayout, QHBoxLayout
@@ -18,7 +19,7 @@ class ContributionWidget(QWidget):
         super().__init__()
         self.date = date
         self.contributions = contributions
-        self.setToolTip(f"Date: {self.date.strftime('%Y-%m-%d')}\LearningTime: {self.contributions * 10} m")
+        self.setToolTip(f"Date: {self.date.strftime('%Y-%m-%d')}\nLearningTime: {self.contributions * 10} m")
     
     def get_contributions(self):
         return self.contributions
@@ -60,25 +61,38 @@ class ContributionCalendar(QMainWindow):
     def initTimer(self):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.__update_contributions)
-        self.timer.start(1000) # 10s更新一次
+        self.timer.start(20 * 1000) # 20s刷新一遍页面
 
-    def __update_contributions(self):
+    def __update_contributions(self) -> None:
         now = datetime.now().date()
-        cnt = self.contributions_data.get(now, 0) + self.__convert_time_to_cnt()
-        self.widgets[now].update_contributions(cnt)
-        self.__dumps_data()
+        cnt = self.contributions_data.get(now, 0) + self.__convert_time_to_cnt() # 缓存时间数据+新增数据计算总时间
+        self.widgets[now].update_contributions(cnt) # 更新数据
+        self.__dumps_data() # 落盘
 
     def __convert_time_to_cnt(self) -> int:
-        return int(self.programTimer.elapsed() / 1000 / 10)
+        """时间转化为cnt，每十分钟计数1
+        """
+        return int(self.programTimer.elapsed() / 1000 / 60 / 10)
 
-    def __get_resource_path(self, relative_path):
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-        return os.path.join(base_path, relative_path)
+    def __get_resource_path(self, relative_path, filename) -> str:
+        """获取缓存文件的路径. 拿到exe文件路径地址之后，拼接文件夹和文件名，文件夹地址没有则创建
+
+        Args:
+            relative_path (_type_): 文件夹名称
+            filename (_type_): 文件名
+
+        Returns:
+            _type_: 返回绝对路径，eg:C:\\xxx\\relative_path\\filename
+        """
+        base_path = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
+        data_dir = os.path.join(base_path, relative_path)
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        return os.path.join(data_dir, filename)
 
     def initUI(self):
+        """初始化ui，负责创建Calendar layout和month_layout，根据缓存文件渲染赋值
+        """
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
 
@@ -88,7 +102,7 @@ class ContributionCalendar(QMainWindow):
         main_hbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
         main_vbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        # 这玩意会定期更新的
+        # 首先加载缓存文件
         self.contributions_data = self.generate_contributions_data()
 
         # Calendar layout that will contain all the month and day widgets
@@ -127,7 +141,7 @@ class ContributionCalendar(QMainWindow):
                 contributions = self.contributions_data.get(day_date.date(), 0)
                 widget = ContributionWidget(day_date, contributions)
                 col = (day + start_day) // 7 + 1  # Calculate row
-                row = (day + start_day) % 7 +1 # Calculate column
+                row = (day + start_day) % 7 + 1 # Calculate column
                 month_layout.addWidget(widget, row, col)
                 self.widgets[day_date.date()] = widget
             # Add month grid to the calendar grid
@@ -136,25 +150,38 @@ class ContributionCalendar(QMainWindow):
         main_vbox.addLayout(main_hbox)
         central_widget.setLayout(main_vbox)
 
-    def generate_contributions_data(self):
-        json_file_path = self.__get_resource_path("data\data.json")
+    def generate_contributions_data(self) -> dict:
+        """获取初始化缓存的文件
+
+        Returns:
+            dict: 缓存的数据：date -> cnt
+        """
+        json_file_path = self.__get_resource_path("data", "data.json")
+        # 文件不存在则创建
+        with open(json_file_path, 'a') as file:
+            pass
         history_data = dict()
         try:
             with open(json_file_path, 'r') as file:
-                data = json.load(file)
+                file_content = file.read().strip()
+                data = json.loads(file_content)
                 for data_str, contributions in data.items():
                     date = datetime.strptime(data_str, '%Y-%m-%d').date()
                     history_data[date] = contributions
+        except json.JSONDecodeError:
+            return {}
         except Exception as e:
-            print(f"UPDATE CONTRIBUTIONS: {e}")
+            raise
         else:
             return history_data
 
     def __dumps_data(self):
+        """将更新的数据写到文件中
+        """
         _d = dict()
         for date, contribution_widget in self.widgets.items():
             _d[str(date)] = contribution_widget.get_contributions()
-        json_file_path = self.__get_resource_path("data\data.json")
+        json_file_path = self.__get_resource_path("data", "data.json")
         with open(json_file_path, 'w') as f:
             json.dump(_d, f, indent=4)
         
